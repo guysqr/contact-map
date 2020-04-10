1a98ce4711fd62137652c380236f122532441807
Guy Morton <guy@alchemy.com.au>
2020-04-11 08:52:12 +1000

    added build folder

---
+++ vue-contact-map/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <amplify-authenticator></amplify-authenticator>
+    <amplify-sign-out></amplify-sign-out>
+  </div>
+</template>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { Auth } from 'aws-amplify'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
