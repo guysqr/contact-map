9bf4f314ed505e409135b0a700bb77f9c8c04630
Guy Morton <guy@alchemy.com.au>
2020-06-05 16:06:38 +1000

    fix API link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
   <footer id="AppFooter">
-      <div class="attribution">
-        Contact Map is an open source project and can be
-        <a
-          class="white"
-          href="https://github.com/guysqr/contact-map"
-          target="_blank"
-        >forked on Github</a>. The
-        <a class="white" href="https://api.contactmap.me/openapi.json" target="_blank">APIs</a> are also available for other developers to use free of charge for public-facing projects (see terms of use).
-      </div>
+    <div class="attribution">
+      Contact Map is an open source project and can be
+      <a
+        class="white"
+        href="https://github.com/guysqr/contact-map"
+        target="_blank"
+      >forked on Github</a>. The
+      <a class="white" href="https://developers.contactmap.me/" target="_blank">APIs</a> are also available for other developers to use free of charge for public-facing projects (see terms of use).
+    </div>
   </footer>
 </template>
 <script>
