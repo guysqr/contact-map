b0461517eb52cf222cb36dd1da3a58e8ca0eeb57
Guy Morton <guy@alchemy.com.au>
2020-07-06 09:23:18 +1000

    added pinpoint

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_content_delivery_bucket_region": "ap-southeast-2",
     "aws_content_delivery_url": "https://d3jsszy9vmlhyj.cloudfront.net",
     "aws_user_files_s3_bucket": "vue-contact-mapabc655a8e874453f89ce44bb2533e3d9map-proto",
-    "aws_user_files_s3_bucket_region": "ap-southeast-2"
+    "aws_user_files_s3_bucket_region": "ap-southeast-2",
+    "aws_mobile_analytics_app_id": "b6a323ab2b56450aa01f1efacdeaf9ee",
+    "aws_mobile_analytics_app_region": "us-west-2"
 };
 
 
