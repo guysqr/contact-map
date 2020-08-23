8675753345e9b93aff5198e0d8f52981b3ed2449
guy <guy@alchemy.com.au>
2020-08-23 10:43:24 +1000

    adding icons

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,7 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    color: #e5eff8;
     margin-top: 60px;
   }
 </style>
