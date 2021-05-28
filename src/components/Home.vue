d76aa9dd33a9aff6f327b0b3da7335f83664b083
Guy Morton <52842774+guysqr@users.noreply.github.com>
2021-05-28 14:48:02 +1000

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@
       <p>Contact Map is designed to allow you to correlate your locations at different times to reported cases by areas.</p>
       <p>It uses public data and combines it with data you upload to allow you to see where you were, in relation to the cases reported over the same times.</p>
       <p>The dots you see on the map are places you stopped at, and the lines show how you got between places. Hover over the dots and lines to see a description. This is all derived from your Google Takeout data.</p>
-      <p><img src="img/sample.jpg" width="960" height="601" /></p>
+      <p><img src="img/sample.jpg" width="960" height="601" alt="sample of UI" /></p>
       <h2>Contact Map doesn't store your private data</h2>
       <p>With Contact Map, you are completely in control of your own data.</p>
       <p>When you upload your Google Takeout Location data to Contact Map, we transform it for use on Contact Map, but we never store your data on our servers, nor do we keep any record of your data.</p>
